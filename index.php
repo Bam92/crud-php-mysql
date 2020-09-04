553d2a436d11a030e43f2b3871cdc070a8eb4f34
Abel Lifaefi Mbula <abelmbula@protonmail.com>
2020-09-04 14:41:18 +0200

    feat: get projects list

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
 
-	<p>Hello, world! The year is <?php echo date("d-m-Y"); ?>.</p>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>theTrackerApp</title>
+</head>
 
+<body>
+    <nav>
+        <h1>theTrackerApp</h1>
 
+        <ul>
+            <li><a href="./project_list.php">Projects list</a></li>
+            <li><a href="./task_list.php">Tasks list</a></li>
+        </ul>
+    </nav>
+    <div>
+        <p>traks time spent on your projects and tasks</p>
+    </div>
+</body>
 
+</html>
